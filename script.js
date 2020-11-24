df653f3251a38577aa2fed661fbc6770234a7e8c
cuencaluis <73263351+cuencaluis@users.noreply.github.com>
2020-11-24 22:56:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let addToButton = document.getElementById("Todo");
+let todoContainer = document.getElementById("todocontainer");
+let inputField = document.getElementById("inputField");
+
+addToButton.addEventListener("click", function(){
+    var paragraph = document.createElement("p");
+    paragraph.innerText = "·"+inputField.value;
+    if(inputField.value.length == 0){
+        return;
+    }
+    todoContainer.appendChild(paragraph);
+    paragraph.classList.add("paragraph-styling");
+    inputField.value = "";
+    paragraph.addEventListener("click", function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener("dblclick", function(){
+        todoContainer.removeChild(paragraph);
+    })
+});
+
+function play(){
+    var tanga = document.getElementById("Todo");
+    var sound = document.getElementById("sounds");
+    
+    sound.play();
+
+}
+
+
